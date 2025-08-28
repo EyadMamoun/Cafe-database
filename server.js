0cf550692b2b56403ba20f758e2b3667d4c528a3
EyadMamoun <eyadmamoun41@gmail.com>
2025-08-28 16:18:04 +0300

    json-server demo

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server running on port ${port}`);
+});
